3bb0c122fd3fb304da11ff9dedb6faf8c05bc38f
Ferreira Lucas <lukasm4a1@gmail.com>
2023-03-22 13:00:03 -0300

    checkpoint
    home cards filtered done

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,31 +1,30 @@
-const {createApp} = Vue
+const { createApp } = Vue
 const url = "./assets/js/amazing.json"
 
-
 createApp({
-    data(){
-        return{
-            events : undefined,
+    data() {
+        return {
+            events: undefined,
             filteredEvents: undefined,
             categories: undefined,
             searchValue: '',
-            checked : []
+            checked: []
         }
     },
-    created(){
+    created() {
         fetch(url)
-            .then( res => res.json() )
+            .then(res => res.json())
             .then(data => {
                 this.events = data.events
                 this.filteredEvents = this.events
-                this.categories = [...new Set(this.events.map(event => event.category))]   
+                this.categories = [...new Set(this.events.map(event => event.category))]
             })
             .catch(err => console.error(err))
     },
-    methods:{
-        filter(){
-            this.filteredEvents = this.events.filter(event => 
-                (this.checked.includes(event.category) || this.checked.length === 0)   
+    methods: {
+        filter() {
+            this.filteredEvents = this.events.filter(event =>
+                (this.checked.includes(event.category) || this.checked.length === 0)
                 && event.name.toLowerCase().includes(this.searchValue.toLowerCase()))
         }
     }
